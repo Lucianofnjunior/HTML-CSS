4ce6234f647709b0242cb5367c9f22209331f561
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2023-11-27 14:31:13 -0300

    Terminando huamburguer menu

---
+++ meus_random/portifolio-demo/js/script.js
@@ -0,0 +1,6 @@
+function togleMenu () {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburguer-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+};
