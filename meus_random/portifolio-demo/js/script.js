dbe6c863ba7708b8bcd5718f53e18728db21f063
Lucianofnjunior <145075484+Lucianofnjunior@users.noreply.github.com>
2023-11-27 21:01:39 -0300

    Starting the profile section

--- meus_random/portifolio-demo/js/script.js
+++ meus_random/portifolio-demo/js/script.js
@@ -1,4 +1,4 @@
-function togleMenu () {
+function toggleMenu () {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburguer-icon');
     menu.classList.toggle('open');
